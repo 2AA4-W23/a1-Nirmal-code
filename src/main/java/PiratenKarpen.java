b6b6848364651ff965b27af702979aa3d0b331fb
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-12 14:34:19 -0500

    Added 42 games in simulation feature
    Added the feature that will allow the players to play 42 games in the
    simulation. Also introduced variables that will keep track of scores in
    each game and overall wins. Player methods used to get this information.

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -22,6 +22,24 @@ public class PiratenKarpen {
         double p2_wins=0;
 
 
+        for (int i=0; i<42; i++) {
+            eight_rolls = myDice.eightRoll();
+            int p1_score = p1.turn(eight_rolls);
+            eight_rolls = myDice.eightRoll();
+            int p2_score = p2.turn(eight_rolls);
+            if (p1_score > p2_score) {
+                p1_wins += 1;
+            } else {
+                p2_wins += 1;
+            }
+
+        }
+
+
+        System.out.printf("Player 1 score: %.3f%%", (p1_wins/42)*100);
+        System.out.println();
+        System.out.printf("Player 2 score: %.3f%%", (p2_wins/42)*100);
+
 
     }
     
