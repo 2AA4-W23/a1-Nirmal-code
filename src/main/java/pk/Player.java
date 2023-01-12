db0df9a6f297c037833b7934ef499b609bb71adc
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-12 14:03:48 -0500

    Added turn method for the player's turn
    Created a turn method in Player class to execute specific functions
    relating to each player's turn. At the moment, turn is executed so that
    the turn ends only when the player obtains 3 skulls (F08).
    
    I will later add something that will randomize which the player keeps to
    finish the method.

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -8,11 +8,34 @@ import java.util.List;
 
 public class Player {
 
+    List <Faces> final_rolls;
+
     public int tally_score(List<Faces> final_rolls){
         int num_gold= Collections.frequency(final_rolls, Faces.GOLD);
-        int num_diamond= Collections.frequency(final_rolls, Faces.GOLD);
+        int num_diamond= Collections.frequency(final_rolls, Faces.DIAMOND);
         int score=(num_gold+num_diamond)*100;
         return score;
     }
-    
+
+    public int turn(List<Faces> starter_roll){
+        int num_skulls=0;
+        Dice myDice=new Dice();
+        starter_roll.removeAll(Collections.singleton(Faces.SKULL));
+        if (8-starter_roll.size()>=3){
+            System.out.println(starter_roll.toString());
+            return tally_score(starter_roll);
+        }else{
+            Random rand=new Random();
+            while (8-starter_roll.size()<3){
+                for (int i=0; i<starter_roll.size();i++){
+
+                }
+                starter_roll.removeAll(Collections.singleton(Faces.SKULL));
+            }
+            System.out.println(starter_roll.toString());
+            return tally_score(starter_roll);
+        }
+
+    }
+
 }
