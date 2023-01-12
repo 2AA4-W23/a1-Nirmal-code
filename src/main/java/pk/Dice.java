4509cb93df699ed6fc0feddaf27720a8cf7a4c26
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-12 13:07:05 -0500

    Created method eightRoll() in Dice.java
    Method created to create the feature F02 that rolls 8 dies in the game.
    Method is created by using the roll() method to generate random faces
    for 8 positions in an ArrayList. Method returns the List.
    
    This completes feature F02.

--- src/main/java/pk/Dice.java
+++ src/main/java/pk/Dice.java
@@ -1,15 +1,25 @@
 package pk;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Dice {
 
     public Faces roll() {
         int howManyFaces = Faces.values().length;
-        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
-        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         Random bag = new Random();
         return Faces.values()[bag.nextInt(howManyFaces)];
+    }
+
+    public List<Faces> eightRoll(){
+        List<Faces> turn_rolls=new ArrayList<Faces>();
+
+        for (int i=0; i<8; i++){
+            turn_rolls.add(roll());
+        }
+
+        return turn_rolls;
+
     }
     
 }
