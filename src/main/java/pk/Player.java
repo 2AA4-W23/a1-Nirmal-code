3deb7634708f44c758fe25c4cb19bd2f2aebcb44
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-16 20:45:24 -0500

    Updated Object-oriented design
    Introduced an init method to initialize each player to have 0 wins and 0
    score. Also made the dice, and rolls specific to each player by making
    it an attribute for each player. This eliminates the need to introduce
    two roll variables in the main file.
    
    Introduced a rerolls() method to break up turn into components. This
    makes it more readable. Also, added a win_update method to remove the
    need to do it in the main file.
    
    Also added necessary comments

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -8,49 +8,93 @@ import java.util.List;
 
 public class Player {
 
-    List <Faces> final_rolls;
+    Dice myDice=new Dice();
 
-    public int tally_score(List<Faces> final_rolls){
-        int num_gold= Collections.frequency(final_rolls, Faces.GOLD);
-        int num_diamond= Collections.frequency(final_rolls, Faces.DIAMOND);
-        int num_monkey= Collections.frequency(final_rolls, Faces.MONKEY);
-        int num_parrot= Collections.frequency(final_rolls, Faces.PARROT);
-        int num_saber= Collections.frequency(final_rolls, Faces.SABER);
+    List <Faces> rolls;
+    public double wins;
+    public int score;
 
-        int score=((num_gold+num_diamond)*100)+(num_gold/3)*100+(num_diamond/3)*100+(num_monkey/3)*100+(num_parrot/3)*100+(num_saber/3)*100;
+
+
+    public Player(){
+        wins=0;
+        score=0;
+    }
+
+    int tally_score(){
+        int num_gold= Collections.frequency(rolls, Faces.GOLD);
+        int num_diamond= Collections.frequency(rolls, Faces.DIAMOND);
+        int num_monkey= Collections.frequency(rolls, Faces.MONKEY);
+        int num_parrot= Collections.frequency(rolls, Faces.PARROT);
+        int num_saber= Collections.frequency(rolls, Faces.SABER);
+
+        score=((num_gold+num_diamond)*100)+(num_gold/3)*100+(num_diamond/3)*100+(num_monkey/3)*100+(num_parrot/3)*100+(num_saber/3)*100;
 
         return score;
     }
 
-    public int turn(List<Faces> starter_roll){
-        int num_skulls=0;
-        Dice myDice=new Dice();
-        starter_roll.removeAll(Collections.singleton(Faces.SKULL));
-        if (8-starter_roll.size()>=3){
-            return tally_score(starter_roll);
-        }else{
-            Random rand=new Random();
-            int fixed_one=rand.nextInt(0,starter_roll.size());
-            int fixed_two=rand.nextInt(0,starter_roll.size());
-            while (8-starter_roll.size()<3){
-                for (int i=0; i<starter_roll.size();i++){
-                    if (i==fixed_one){
-                        starter_roll.set(i, myDice.roll());
-                    }else if (i==fixed_two){
-                        starter_roll.set(i, myDice.roll());
-                    }else{
-                        int reroll=rand.nextInt(0,2);
-                        if (reroll==1) {
-                            starter_roll.set(i, myDice.roll());
-                        }
+    void rerolls() {
+        Random rand = new Random();
+
+        //selects which two die will be rerolled from die left (at minimum).
+        int fixed_one = rand.nextInt(0, rolls.size());
+        int fixed_two;
+        do{
+            fixed_two = rand.nextInt(0, rolls.size());
+        }while (fixed_two!=fixed_one);
+
+        //condition: true while there are less than 3 dies removed.
+        while (8 - rolls.size() < 3) {
+
+            for (int i = 0; i < rolls.size(); i++) {
+
+                if (i == fixed_one) {
+                    rolls.set(i, myDice.roll());
+                } else if (i == fixed_two) {
+                    rolls.set(i, myDice.roll());
+                } else {
+                    //If dice isn't fixed to be rerolled, this randomizes whether the dice should be rerolled.
+                    int reroll = rand.nextInt(0, 2);
+
+                    if (reroll == 1) {
+                        rolls.set(i, myDice.roll());
                     }
                 }
-                starter_roll.removeAll(Collections.singleton(Faces.SKULL));
             }
+            rolls.removeAll(Collections.singleton(Faces.SKULL));
+        }
+    }
 
-            return tally_score(starter_roll);
+    public int turn(){
+
+        //new roll each turn
+        rolls =myDice.eightRoll();
+
+        rolls.removeAll(Collections.singleton(Faces.SKULL));
+
+        //checks if 3 die or more are not already skulls.
+        if (8-rolls.size()>=3){
+            tally_score();
+            return score;
+        }else{
+            //if less than 3 are skulls, this rerolls.
+            rerolls();
+            tally_score();
+            return score;
         }
 
+    }
+
+
+    public static void win_update(Player p1, Player p2){
+
+        if (p1.score >= p2.score) {
+            p1.wins += 1;
+            System.out.println("Player 1 Wins!");
+        } else {
+            p2.wins += 1;
+            System.out.println("Player 2 Wins!");
+        }
     }
 
 }
