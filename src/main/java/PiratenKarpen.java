d495c6c785943e76314d50db28bb9476ef2f288c
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-16 20:49:25 -0500

    Simplified code as consequence of prev commit
    Removed unecessary variables that were made specific to each player in
    the Player.java class. Also updates each round, and the scores during
    each turn and at end.

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,10 +1,6 @@
-import pk.Dice;
-import pk.Faces;
+
 import pk.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 
 public class PiratenKarpen {
@@ -12,33 +8,28 @@ public class PiratenKarpen {
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling a dice");
-        Dice myDice = new Dice();
 
         Player p1=new Player();
         Player p2=new Player();
-        List<Faces> eight_rolls;
 
-        double p1_wins=0;
-        double p2_wins=0;
 
 
         for (int i=0; i<42; i++) {
-            eight_rolls = myDice.eightRoll();
-            int p1_score = p1.turn(eight_rolls);
-            eight_rolls = myDice.eightRoll();
-            int p2_score = p2.turn(eight_rolls);
-            if (p1_score > p2_score) {
-                p1_wins += 1;
-            } else {
-                p2_wins += 1;
-            }
+
+            p1.score+= p1.turn();
+            p2.score+= p2.turn();
+
+            System.out.printf("Player 1: %d \n", p1.score);
+            System.out.printf("Player 2: %d \n", p2.score);
+
+            Player.win_update(p1,p2);
+
 
         }
 
 
-        System.out.printf("Player 1 score: %.3f%%", (p1_wins/42)*100);
-        System.out.println();
-        System.out.printf("Player 2 score: %.3f%%", (p2_wins/42)*100);
+        System.out.printf("Player 1 wins: %.0f ; score: %d ; win_percentage: %.3f%%\n", p1.wins, p1.score, (p1.wins/42)*100);
+        System.out.printf("Player 2 wins: %.0f ; score: %d ; win_percentage: %.3f%%\n", p2.wins, p2.score, (p2.wins/42)*100);
 
 
 
