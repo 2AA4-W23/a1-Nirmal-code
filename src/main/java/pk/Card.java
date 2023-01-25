db7f4717022ce16ff0ef787d1e132e1ee76460c6
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-25 12:38:55 -0500

    Added a get method for Card
    Created a get method for each of the card attribute. Done because they
    are made private.

--- src/main/java/pk/Card.java
+++ src/main/java/pk/Card.java
@@ -15,4 +15,25 @@ public class Card {
         this.points=p;
     }
 
+    public int getVal(){
+        if (this.val==CardValues.ONE){
+            return 1;
+        }else if (this.val==CardValues.TWO){
+            return 2;
+        }else if (this.val==CardValues.THREE){
+            return 3;
+        }else if (this.val==CardValues.FOUR){
+            return 4;
+        }else{
+            return 0;
+        }
+    }
+
+    public CardTypes getType(){
+        return this.type;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
 }
