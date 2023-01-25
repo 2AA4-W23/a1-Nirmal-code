831af49b9001880cb29e7a1a250709e0f6aee3b3
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-25 12:46:11 -0500

    Introduced a card deck
    Created a class CardDeck that extends the Card class. In the
    constructor, the class adds all the required cards to the queue deck.
    So, contructor is used as a new card deck each time.

---
+++ src/main/java/pk/CardDeck.java
@@ -0,0 +1,27 @@
+package pk;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
+public class CardDeck extends Card {
+
+    Queue<Card> deck=new ArrayDeque<Card>();
+
+    public CardDeck(){
+
+        for (int i=0; i<29; i++){
+            deck.add(new Card(CardTypes.Unknown, CardValues.NONE, 0));
+        }
+
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.TWO, 300));
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.TWO,300));
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.THREE,500));
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.THREE,500));
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.FOUR,1000));
+        deck.add(new Card(CardTypes.SeaBattle, CardValues.FOUR,1000));
+    }
+
+}
